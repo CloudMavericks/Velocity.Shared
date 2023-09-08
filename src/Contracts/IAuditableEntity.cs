28c3fee9857313cca9fd507019dd20e638eeb11b
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-09-08 12:39:08 +0530

    Add Contract interfaces

---
+++ src/Contracts/IAuditableEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Velocity.Shared.Contracts;
+
+public interface IAuditableEntity : IEntity
+{
+    DateTime CreatedAt { get; set; }
+    Guid CreatedBy { get; set; }
+    string CreatedByName { get; set; }
+    
+    DateTime? UpdatedAt { get; set; }
+    Guid? UpdatedBy { get; set; }
+    string UpdatedByName { get; set; }
+    
+    DateTime? DeletedAt { get; set; }
+    Guid? DeletedBy { get; set; }
+    string DeletedByName { get; set; }
+}
+
+public interface IAuditableEntity<T> : IAuditableEntity, IEntity<T>
+{
+    
+}
---
+++ src/Contracts/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Velocity.Shared.Contracts;
+
+public interface IEntity
+{
+    
+}
+
+public interface IEntity<T> : IEntity
+{
+    T Id { get; set; }
+}
