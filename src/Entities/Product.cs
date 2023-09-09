96c716ebd2ff4b3b8e746674738289ad7b68893e
SathiyaramanM <Sathiyaraman2003@gmail.com>
2023-09-09 14:58:40 +0530

    Update Product entity with basic info

--- src/Entities/Product.cs
+++ src/Entities/Product.cs
@@ -1,6 +1,11 @@
+using Velocity.Shared.Contracts;
+
 namespace Velocity.Shared.Entities;
 
-public class Product
+public class Product : IEntity
 {
     public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Code { get; set; }
 }
